d0b8ae61c241b7971a66122fea6d23aa9a67f311
Martim <mlm.martim@hotmail.com>
2019-05-22 17:23:27 +0100

    segundo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HOME' });
 });
+router.get("/login", function(req, res){
+  res.render("login");
+})
 
 module.exports = router;
